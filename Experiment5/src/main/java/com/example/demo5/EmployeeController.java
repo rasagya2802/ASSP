2ac8fbfdc27b7922f56b7312b62e73b7470a6219
rasagya2802 <gaderasagya1@gmail.com>
2025-04-02 22:55:46 +0530

    removed unnecessary import statements

--- Experiment5/src/main/java/com/example/demo5/EmployeeController.java
+++ Experiment5/src/main/java/com/example/demo5/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.example.demo5;
 
-import com.example.demo5.Employee;
-import com.example.demo5.EmployeeService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
